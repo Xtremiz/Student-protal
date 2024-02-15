eee356654b3d381566ec9f7c97417d55f1226d76
Xtremiz <fozanahmed01@gmail.com>
2024-02-15 14:14:24 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,6 +2,15 @@ import 'dart:io';
 
 void main() {
   stdout.write("Enter your name: ");
-  var name = stdin.readLineSync();
-  print("Hello, $name!");
+  String? name = stdin.readLineSync();
+
+  stdout.write("Enter your password: ");
+  String? password = stdin.readLineSync();
+
+  if (name != null && password != null) {
+    print("Name: $name");
+    print("Password: $password");
+  } else {
+    print("Invalid input.");
+  }
 }
