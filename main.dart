19c595a2bfb5a03bd088139a70ce333a8aec6d21
Xtremiz <fozanahmed01@gmail.com>
2024-02-16 17:48:05 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,50 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Enter your name: ");
+  stdout.write("Enter your email: ");
   String? name = stdin.readLineSync();
 
   stdout.write("Enter your password: ");
   String? password = stdin.readLineSync();
 
   if (name == "g" && password == "g") {
-    print("login");
+    print("login successful");
+
+    stdout.write("Enter your marks obtained: ");
+    String? marksInput = stdin.readLineSync();
+    int? marksobtained = int.tryParse(marksInput ?? '');
+
+    stdout.write("Enter your total marks: ");
+    String? totalMarksInput = stdin.readLineSync();
+    int? totalmarks = int.tryParse(totalMarksInput ?? '');
+
+    if (marksobtained != null && totalmarks != null && totalmarks != 0) {
+      double percentage = (marksobtained / totalmarks) * 100;
+      print("Marks obtained: $marksobtained");
+      print("Total marks: $totalmarks");
+      print("Percentage: $percentage");
+      print(
+          "from your $percentage% your passing status is ${grade(percentage)}");
+    } else {
+      print("Invalid marks or total marks.");
+    }
   } else {
-    print("fail");
+    print("Login failed");
+  }
+}
+
+grade(double number) {
+  if (number >= 80) {
+    return "A+ ";
+  } else if (number >= 70) {
+    return "A";
+  } else if (number >= 60) {
+    return "B";
+  } else if (number >= 50) {
+    return "C";
+  } else if (number >= 40) {
+    return "D";
+  } else {
+    return "fail";
   }
 }
